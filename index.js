36f701c69ff5505137cd05766eb3589382749be1
zainkhan1726 <160050990+zainkhan1726@users.noreply.github.com>
2024-03-23 02:17:27 +0500

    commited number game

---
+++ index.js
@@ -0,0 +1,15 @@
+#! /usr/bin/env node
+import inquirer from "inquirer";
+import chalk from "chalk";
+const randomNumber = Math.floor(Math.random() * 6 + 1);
+const answer = await inquirer.prompt({
+    name: "userNumber",
+    type: "number",
+    message: "Guess a number between 1-6.",
+});
+if (answer.userNumber === randomNumber) {
+    console.log(chalk.blue("Congragulations! You guessed the right number."));
+}
+else {
+    console.log(chalk.blue("Better luck next time.Try again."));
+}
